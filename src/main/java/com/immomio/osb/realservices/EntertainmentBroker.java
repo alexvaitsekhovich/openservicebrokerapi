fe2285ed23810905faddaa66b784e43adacbdcee
alexvaitsekhovich <G1tHub-8>
2021-06-07 00:36:57 +0200

    Updated GitLAb Ci config

--- src/main/java/com/immomio/osb/realservices/EntertainmentBroker.java
+++ src/main/java/com/immomio/osb/realservices/EntertainmentBroker.java
@@ -29,7 +29,7 @@ public class EntertainmentBroker implements Broker {
         provisionedServices = new HashMap<>();
         serviceBindings = new HashMap<>();
 
-        var services = initServices();
+        List<ServiceSpec> services = initServices();
         servicesContainer = new ServiceSpecsContainer(services);
     }
 
